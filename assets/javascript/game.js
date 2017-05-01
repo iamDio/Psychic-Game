8f4bda81e5db344ed8f885933a0a8d9a569dffdb
iamDio <claudio.villani00@gmail.com>
2017-05-01 18:14:51 -0400

    Fixed array that shows guessed letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,40 +9,52 @@
     var losses = 0;
     var guessesLeft = 10;
 
+//==========================================
+// FUNCTIONS
+//==========================================
+//function that 
+
 //==============================================
 // EVENT LISTENER/MAIN CODE
 //==============================================
 //listens for keypress and prints it guessedLetters
     addEventListener("keydown", function(event) {
 
-//when a key is pressed its stored in var userGuess
+      //when a key is pressed its stored in var userGuess
         var userGuess = event.key; 
         var compGuess = allLetters[Math.floor(Math.random() * allLetters.length)];
         console.log(compGuess);
         console.log(event.key);
 
-     //UPDATES SCORE BASED ON CORRECT OR INCORRECT GUESS
+      //stores event.key in guesses array and prints it to the screen
+        guesses.push(event.key);   
+        document.querySelector('#guessedLetters').innerHTML = "Guessed letters: " + guesses;
+        console.log(guesses);
+
+      //UPDATES SCORE BASED ON CORRECT OR INCORRECT GUESS
         if (compGuess === event.key) {
             wins++;
         } else if (compGuess != event.key) {
             losses++;
             guessesLeft--;
-             document.querySelector('#guessedLetters').innerHTML = "guesses " + guesses[event.key]; //prints guessed letters 
-    
+    //prints guessed letters
+
+            
     //PRINTS RESULTS OF IF STATEMENTS TO HTML #game <div>
             var html = "<p>wins: " + wins + "</p>" +
                 "<p> losses: " + losses + "</p>" +
-                "<p> guesses left: " + guessesLeft + "</p>";
+                "<p> guesses left: " + guessesLeft + "</p>"
+                "<p> guessed letters: "+ guesses + "</p>";
 
             document.querySelector('#game').innerHTML = html;
         }
-    
+
      //RESET VARIABLES TO ORIGINAL VALUES WHEN CONDITIONS ARE MET 
         if (guessesLeft === 0) {
             wins = 0;
             losses = 0;
             guessesLeft = 10;
-
+            guesses = [];
      //PRINTS RESET VALUES TO #game <div>
             var pageReset = "<p>wins: " + wins + "</p>" +
                 "<p> losses: " + losses + "</p>" +
